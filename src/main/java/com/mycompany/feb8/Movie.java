0c683b46a1742e934f03c7bd4f682f585d93ada9
sushantshambharkar <40300856+sushantshambharkar@users.noreply.github.com>
2020-02-08 13:00:22 -0600

    Add files via upload

---
+++ src/main/java/com/mycompany/feb8/Movie.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.feb8;
+
+/**
+ *
+ * @author Sushant
+ */
+public class Movie {
+
+    public String getMoviename() {
+        return moviename;
+    }
+
+    public void setMoviename(String moviename) {
+        this.moviename = moviename;
+    }
+
+    public Integer getMovieid() {
+        return movieid;
+    }
+
+    public void setMovieid(Integer movieid) {
+        this.movieid = movieid;
+    }
+    
+    String moviename;
+    Integer movieid;
+    
+    
+}
---
+++ src/test/java/MovieTest.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import com.mycompany.feb8.Movie;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Sushant
+ */
+public class MovieTest {
+    
+    Movie movie;
+    
+    public MovieTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+        
+        movie = new Movie();
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+     @Test
+     public void testmovie() {
+     movie.setMovieid(1);
+     movie.setMoviename("DDLJ");
+     
+    assertEquals("DDLJ",movie.getMoviename());
+    assertEquals("1",movie.getMovieid().toString());
+     }
+}
